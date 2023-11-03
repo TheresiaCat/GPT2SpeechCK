05ef344d06194ae0b24db7b9160ce476dd2c213d
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-11-03 17:38:14 +0100

    loadPHPContet Nachricht senden und empfangen
    Gesamtes Projekt in PHP verschoben, URLs entspechend angepasst, fetchAPI statt xml HTTP-Requests, demo Funktion ersetzt

--- script.js
+++ script.js
@@ -5,31 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const sendButton = document.querySelector("#send-button");
     const chatContainer = document.querySelector("#chat-container");
 
-    const demoForm = document.getElementById('demoForm');
-    const responseMessage = document.getElementById('responseMessage');
-    
-    demoForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-    
-        const messageInput = document.getElementById('demoMessage');
-        const demomessage = messageInput.value;
-    
-        // AJAX-Anfrage an das PHP-Skript senden
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://localhost/GPT2SpeechPHP/api.php', true);  
-        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                responseMessage.innerHTML = xhr.responseText;
-            }
-        };
-        xhr.send('demoMessage=' + encodeURIComponent(demomessage));
-    });
-    
-
-
-
-
     // Bei Button-Klicken oder Enter-Taste wird der Inhalt des Inputfeldes übergeben an die Funktion "sendMessage"
     sendButton.addEventListener("click", sendMessage);
     messageInput.addEventListener("keydown", function (event) {
@@ -46,7 +21,11 @@ document.addEventListener("DOMContentLoaded", function () {
         if (userMessage !== "") {
             // Sende Nachricht an die API und erhalte eine Antwort (fehlt noch)
             // Beispielantwort von der API
-            loadPhpContent(function (botResponse) {
+            const formdata = new FormData()
+            formdata.append("demoMessage",userMessage)
+            const urlformdata = new URLSearchParams(formdata)
+            loadPhpContent(urlformdata, function (botResponse) {
+            
 
                 // Erstelle einen neuen qna-container für diese Runde
                 const qnaContainer = document.createElement("div");
@@ -76,35 +55,19 @@ document.addEventListener("DOMContentLoaded", function () {
         qnaContainer.appendChild(messageElement);
     }
 
-
-    function loadPhpContent(callback) {
-        let xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "http://localhost/GPT2SpeechPHP/api.php", true);
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                let test = this.responseText;
-                callback(test); //wartet bis variable einen Wert hat
-
-            }
-        };
-        xhttp.send();
-    }
-
-
-    // Auf Mutationen im DOM zu reagieren
-    let targetNode = document.getElementById("bot-message");
-    let observer = new MutationObserver(function (mutationsList) {
-        for (let mutation of mutationsList) {
-            if (mutation.type == "childList") {
-                // Hier kannst du zusätzliche Aktionen ausführen, wenn sich der DOM ändert
-            }
+    //Anfrage an PHP mit Übergabe von Text und Entgegennehmen von echo 
+    async function loadPhpContent(formdata, callback) {
+        const response = await fetch("/PHP/api.php",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: formdata
+        })
+        if(response.ok){
+            response.text().then((text)=>{
+                callback(text)
+            })
         }
-    });
-    let config = {
-        attributes: true,
-        childList: true,
-        subtree: true
-    };
-    observer.observe(targetNode, config);
-
+    }
 });
