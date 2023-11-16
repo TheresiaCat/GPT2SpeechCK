4db9b543dbc2236b78b9725097f2fa8314ad4142
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-11-16 15:37:08 +0100

    Cookies zur Sessionspeicherung

--- PHP/login.php
+++ PHP/login.php
@@ -3,8 +3,6 @@ include 'query.php';
 
 $username = $_POST['username'];
 $password = $_POST['password'];
-//$_SESSION["test"].=$username;
-//die($_SESSION["test"]); 
 $mysqli = connectToDatabase();
 
 $result = queryDatabaseWithParameters($mysqli, "SELECT * FROM BENUTZER WHERE BENUTZERNAME = ? AND PASSWORT = ?", array($username, $password), 'ss');
@@ -12,6 +10,7 @@ $counter = $result->num_rows;
 
 if ($counter == 1) {
     session_start();
+     
 
     //autologout nach 10 minuten
     $_SESSION['logged_in'] = true; 
@@ -21,6 +20,9 @@ if ($counter == 1) {
 
     $row = mysqli_fetch_array($result);
     $_SESSION['userid'] = $row['BenutzerID'];
+
+    $cookie = base64_encode($row['Benutzername'].":".$row['BenutzerID']); 
+    setcookie("phpSessionCookie", $cookie, time()+60*60*24*30, "/");
 
     header("Location: /"); // Weiterleitung zur index.html
     exit(); // Wichtig, um sicherzustellen, dass der Code nach der Weiterleitung nicht weiter ausgeführt wird
