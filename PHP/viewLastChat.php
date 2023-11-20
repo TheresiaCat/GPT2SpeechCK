b6f15995b0a8da4e6cd395ee05fdfd7afa19aad6
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-11-20 18:55:34 +0100

    Error handling: keine Chats

--- PHP/viewLastChat.php
+++ PHP/viewLastChat.php
@@ -1,24 +1,29 @@
-<link rel="stylesheet" href="../style.css">
+<link rel="stylesheet" href="../mystyle.css">
 <?php
 include 'query.php';
 session_start();
 
 $result = queryDatabaseWithParameters($mysqli, "SELECT * FROM chats WHERE BenutzerID = ? ORDER BY ChatID DESC LIMIT 1 ", array($_SESSION['userid']), 's');
 
-$serializedContent = mysqli_fetch_array($result);
+if($result->num_rows != 0){
+    $serializedContent = mysqli_fetch_array($result);
+    $unserializedContent = unserialize($serializedContent['Nachrichten']);
+    $length = count($unserializedContent->user);
 
-$unserializedContent = unserialize($serializedContent['Nachrichten']); 
-//print_r($unserializedContent->user);
+    for ($i = 0; $i<$length; $i++){
+        print_r('<div class="lc" id="user-bubble">'); 
+        echo $unserializedContent->user[$i]; 
+        print_r('</div>');
 
-$length = count($unserializedContent->user); 
-
-for ($i = 0; $i<$length; $i++){
-    print_r('<div id="user-bubble">'); 
-    echo $unserializedContent->user[$i]; 
-    print_r('</div><div id="bot-bubble">');
-    echo $unserializedContent->bot[$i];
-    print_r('</div>');
+        print_r('<div class="lc"id="bot-bubble">');
+        echo $unserializedContent->bot[$i];
+        print_r('</div>');
+    }
 }
-print_r('<form action="index.php"><input type="submit" value="Back to Chat"></form>');
+else{
+    print_r('<p>No Messages<p>');  
+}
+ 
+print_r('<form class="sidebar-container" action="index.php"><input type="submit" value="Back to Chat"></form>');
 ?>
 
